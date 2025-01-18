d7c6de77274f66b15c448a60f5ea321318174eba
NurmuhammadAkhmedov <ahmedov13nurmuhammad@gmail.com>
2025-01-18 16:00:22 +0500

    Add files via upload

---
+++ apple.js
@@ -0,0 +1,11 @@
+let str = prompt ("String typedagi malumot kiriting")
+let num = +prompt ("Number typedagi malumot kiriting")
+let boo = +prompt ("Boolean typedagi malumot kiriting")
+
+alert(
+    `
+     hamma malumotlarni ${str} to'g'ri kiritganizga ishonchingiz komilmi?
+    hamma malumotlarni ${num}  to'g'ri kiritganizga ishonchingiz komilmi?
+    hamma malumotlarni ${boo}  to'g'ri kiritganizga ishonchingiz komilmi?
+    `
+)
